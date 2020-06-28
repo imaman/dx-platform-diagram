5b47aabbe879fd24dd6fe7678cf1d0e7da2fe444
Itay Maman <imaman@users.noreply.github.com>
2020-06-28 18:06:55 +0300

    create a graph

---
+++ index.js
@@ -0,0 +1,30 @@
+const util = require('util')
+const graphviz = require('graphviz');
+
+// Create digraph G
+var g = graphviz.digraph("G");
+
+// Add node (ID: Hello)
+// var n1 = g.addNode( "Hello", {"fillcolor" : "red"} );
+// n1.set( "style", "filled" );
+
+
+var n1 = g.addNode("Outlets")
+
+// Add node (ID: World)
+var n2 = g.addNode("DxInfrasturcture");
+
+// Add edge between the two nodes
+var e = g.addEdge(n1, n2);
+e.set( "color", "red" );
+
+
+
+
+
+
+// console.log( g.to_dot() );
+
+
+// Generate a PNG output
+g.output( "png", "test01.png" );
