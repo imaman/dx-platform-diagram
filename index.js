e45f329707a20c52f1ba78bee38bc59c58957c4c
Itay Maman <imaman@users.noreply.github.com>
2020-06-30 17:04:05 +0300

    two classifiers

--- index.js
+++ index.js
@@ -8,13 +8,72 @@ if (!fs.existsSync(outdir)) {
   fs.mkdirSync(outdir)
 }
 
-// Add node (ID: Hello)
-// var n1 = g.addNode( "Hello", {"fillcolor" : "red"} );
-// n1.set( "style", "filled" );
+
+function buildEdges(g, shapeById, d, blockClassifier, shapeClassifier) {
+
+  function resolve(v, f) {
+    while (true) {
+      const next = f(v) || v
+      if (next === v) {
+        return v
+      }
+      v = next
+    }
+  }
+
+  const vertices = new Set()
+  d.forEach(curr => {
+    curr.forEach(v => {
+      v = resolve(v, blockClassifier)
+      if (vertices.has(v)) {
+        return
+      }
+
+      vertices.add(v)
+      const shape = shapeById[resolve(v, shapeClassifier)]
+      if (!shape) {
+        throw new Error(`No shape found for "${v}"`)
+      }
+      g.addNode(v, {shape})
+    })
+  })
+
+  const edges = new Set()
+  d.forEach(curr => {
+    curr.slice(1).forEach(n => {
+      const from = resolve(curr[0], blockClassifier)  
+      const to = resolve(n, blockClassifier)
+      if (from === to) {
+        return
+      }
+      const combined = JSON.stringify([from, to])
+      if (edges.has(combined)) {
+        return
+      }
+      edges.add(combined)
+      const e = g.addEdge(from, to)
+      e.set('color', 'red')
+    })
+  })  
+}
+
+
+function draw(filename, meta, outgoing, blockClassifier, shapeClassifier) {
+  if (!blockClassifier) {
+    throw new Error(`classify cannot be falsy`)
+  }
+  if (!shapeClassifier) {
+    throw new Error(`classify2 cannot be falsy`)
+  }
+  var g = graphviz.digraph("G");
+  buildEdges(g, meta, outgoing, blockClassifier, shapeClassifier)
+  g.output('svg', `${outdir}/${filename}.svg`);  
+}
+
 const shapeById = {
   User: 'plain',
   Github: 'circle',
-  CloudProviders: 'circle',
+  "CloudProvider(s)": 'circle',
   System: 'circle',
   AWS: 'circle',
   GAE: 'circle',
@@ -50,92 +109,42 @@ const shapeById = {
   //
 
   ArtifactRegistry: 'cylinder'
-
-
-
-
 }
 
-function buildEdges(g, shapeById, d, classify = x => x) {
-
-  function resolve(v) {
-    while (true) {
-      const next = classify(v) || v
-      if (next === v) {
-        return v
-      }
-      v = next
-    }
-  }
-
-  const vertices = new Set()
-
-  if (!d) {
-    throw new Error(`d=${JSON.stringify(d)}, shapebyid=${JSON.stringify(shapeById)}`)
-  }
-  d.forEach(curr => {
-    curr.forEach(v => {
-      v = resolve(v)
-      if (vertices.has(v)) {
-        return
-      }
-
-      vertices.add(v)
-      const shape = shapeById[v]
-      if (!shape) {
-        throw new Error(`No shape found for "${v}"`)
-      }
-      g.addNode(v, {shape})
-    })
-  })
-
-  const edges = new Set()
-  d.forEach(curr => {
-    curr.slice(1).forEach(n => {
-      const from = resolve(curr[0])  
-      const to = resolve(n)
-      if (from === to) {
-        return
-      }
-      const combined = JSON.stringify([from, to])
-      if (edges.has(combined)) {
-        return
-      }
-      edges.add(combined)
-      const e = g.addEdge(from, to)
-      e.set('color', 'red')
-    })
-  })  
+const classOf = {
+  AWS: 'CloudProvider(s)',
+  AppDefinitonService: 'DxPlatform',
+  ArtifactRegistry: null,
+  BazelBuild: 'BuildPlatform',
+  BuildDefinitionService: 'DxPlatform',
+  BuildOutputService: 'BuildPlatform',
+  BuildRunService: 'BuildPlatform',
+  FalconBuild: 'BuildPlatform',
+  GAE: 'CloudProvider(s)',
+  GCB: 'BuildEngine(s)',
+  GcbAgent: 'BuildPlatform',
+  TimelineService: 'DxPlatform',
+  Github: null,
+  Lifecycle: 'Outlets',
+  RolloutService: 'Production',
+  System: 'CloudProvider(s)',
+  TC: 'BuildEngine(s)',
+  TcAgent: 'BuildPlatform',
+  TriggeringService: 'BuildPlatform',
+  BuildController: 'BuildPlatform',
+  User: null
 }
-
-
-function draw(filename, meta, outgoing, classify) {
-  var g = graphviz.digraph("G");
-  buildEdges(g, meta, outgoing, classify)
-  g.output('svg', `${outdir}/${filename}.svg`);  
-}
-
-
-// draw("highlevel", shapeById, [
-//   ["User", "Outlets", "Github", "ArtifactRegistry"],
-//   ["Outlets", "Production", "BuildPlatform", "DxPlatform"],
-//   ["BuildPlatform", "DxPlatform", "BuildEngine(s)", "ArtifactRegistry"],
-//   ["Production", "DxPlatform", "CloudProviders", "BuildPlatform"],
-//   ["CloudProviders", "ArtifactRegistry"],
-//   ["BuildEngine(s)", "BuildPlatform"],
-//   ["Github", "BuildPlatform"],
-// ]);
-
 
 
 const all = [
   ["User", "Lifecycle", "Github", "ArtifactRegistry"],
-  ["Lifecycle", "AppDefinitonService", "BuildRunService", "RolloutService", "TimelineService"],
-  ["RolloutService", "System", "AWS", "GAE", "BuildOutputService", "TimelineService"],
+  ["Lifecycle", "AppDefinitonService", "BuildRunService", "Production", "TimelineService"],
+  ["Production", "System", "AWS", "GAE", "BuildOutputService", "TimelineService"],
   ["BuildOutputService", "ArtifactRegistry"],
   ["Github", "TriggeringService"],
   ["TriggeringService", "BuildController"],
-  ["BuildController", "BuildDefinitionService", "BuildRunService", "FalconBuild", "BazelBuild", "TimelineService"],
+  ["BuildController", "BuildDefinitionService", "BuildRunService", "FalconBuild", "BazelBuild"],
+  ["BuildRunService", "TimelineService"],
   ["BazelBuild", "GCB"],
   ["FalconBuild", "TC"],
   ["TC", "TcAgent"],
@@ -148,37 +157,12 @@ const all = [
 ]
 
 
-const classOf = {
-  AWS: 'CloudProviders',
-  AppDefinitonService: 'DxPlatform',
-  ArtifactRegistry: null,
-  BazelBuild: 'BuildPlatform',
-  BuildDefinitionService: 'DxPlatform',
-  BuildOutputService: 'BuildPlatform',
-  BuildRunService: 'BuildPlatform',
-  FalconBuild: 'BuildPlatform',
-  GAE: 'CloudProviders',
-  GCB: 'BuildEngine(s)',
-  GcbAgent: 'BuildPlatform',
-  TimelineService: 'DxPlatform',
-  Github: null,
-  Lifecycle: 'Outlets',
-  RolloutService: 'Production',
-  System: 'CloudProviders',
-  TC: 'BuildEngine(s)',
-  TcAgent: 'BuildPlatform',
-  TriggeringService: 'BuildPlatform',
-  BuildController: 'BuildPlatform',
-  User: null
-}
-
-
 const set = new Set()
 all.forEach(curr => curr.forEach(v => set.add(v)))
 
 console.log([...set].sort().join('\n'))
 
-draw("highlevel", shapeById, all, x => classOf[x])
-draw("fine", shapeById, all);
+draw("highlevel", shapeById, all, x => classOf[x], x => classOf[x])
+draw("fine", shapeById, all, x => x, x => classOf[x]);
 
 
